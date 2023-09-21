953ca29b6f356397d04010df45adc1f544392b95
npmrun <1549469775@qq.com>
2023-09-22 01:01:38 +0800

    add

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,9 @@
-import fs from "fs"
+import fs from "fs-extra"
+const pkgJSON = fs.readJSONSync("packages/law-ui/package.json")
+const rootPkgJSON = fs.readJSONSync("package.json")
 
-fs.createReadStream('./scripts/package.json.1').pipe(
-    fs.createWriteStream('./dist/law-ui/package.json')
-)
+pkgJSON.peerDependencies = rootPkgJSON.peerDependencies
+
+fs.writeJSONSync("./dist/law-ui/package.json", pkgJSON, { spaces: 2 })
+
+fs.createReadStream("./readme.md").pipe(fs.createWriteStream("./dist/law-ui/readme.md"))
